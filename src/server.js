ca31bcbfa45a4c1ca469423cbb9872eb661017bd
Damien Chambon <damien.chambon.pro@gmail.com>
2025-05-28 18:08:14 +0200

    Fix PORT issue

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import app from './index.js';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.SERVER_PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
