2dd9ac7aaf416097bfff9b37cdb08c63420ec838
Damien Chambon <damien.chambon.pro@gmail.com>
2025-05-28 07:57:38 +0200

    Fix syntax issue

--- src/config/database.js
+++ src/config/database.js
@@ -1,4 +1,4 @@
-const { Sequelize } = require('sequelize');
+import { Sequelize } from 'sequelize';
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
@@ -18,4 +18,4 @@ const sequelize = new Sequelize(
   }
 );
 
-module.exports = sequelize;
+export default sequelize;
