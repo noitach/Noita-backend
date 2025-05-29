91dfabaf684a9202a25ce4f4066079ddcad87136
Damien Chambon <damien.chambon.pro@gmail.com>
2025-05-29 21:32:41 +0200

    Make DB_HOST an env variable with a default to localhost

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ import { Sequelize } from 'sequelize';
 
 // Database connection we want to use underscored naming strategy
 const sequelize = new Sequelize(
-  `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@localhost:5432/${process.env.DB_NAME}`,
+  `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST || 'localhost'}:${process.env.DB_PORT || '5432'}/${process.env.DB_NAME}`,
   {
     define: {
       underscored: true,
